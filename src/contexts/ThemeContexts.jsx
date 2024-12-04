c081d871cd9b599059eb33522bbd5aac6eea048a
aznshrllh <ahmadzakinashirullah@gmail.com>
2024-12-04 21:24:32 +0700

    fix: hover on several pages

--- src/contexts/ThemeContexts.jsx
+++ src/contexts/ThemeContexts.jsx
@@ -12,13 +12,13 @@ export function ThemeProvider({ children }) {
     light: {
       navbar: "bg-gradient-to-r from-blue-400 via-purple-350 to-pink-500",
       text: "text-gray-800",
-      button: "bg-blue-500 text-white hover:bg-blue-400",
+      button: "bg-blue-500 text-white ",
       background: "bg-gradient-to-r from-teal-200 via-indigo-200 to-purple-200",
     },
     dark: {
       navbar: "bg-gradient-to-r from-black via-gray-800 to-blue-800",
       text: "text-gray-200",
-      button: "bg-gray-700 text-gray-300 hover:bg-gray-600",
+      button: "bg-gray-700 text-gray-300 ",
       background: "bg-gray-900",
     },
   };
