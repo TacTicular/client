76934a5ecffe2d1c7c7e4b1133cace611d57031a
yotamputra <yotamppy@gmail.com>
2024-12-04 11:02:32 +0700

    feat: connect socket.io from client

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import socket from "../socket";
+
 export default function Play() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.auth = {
+      username: localStorage.username,
+    };
+    socket.connect();
+
+    socket.on("users:online", (onlineUsers) => {
+      setUsers(onlineUsers);
+    });
+
+    return () => {
+      socket.off("users:online");
+      socket.disconnect();
+    };
+  }, []);
+
+  console.log(users)
+
+
   return <div>test</div>;
 }
