e273b166c464940be5a551174585eef6130455f8
aznshrllh <ahmadzakinashirullah@gmail.com>
2024-12-04 10:40:05 +0700

    feat: set up socket client

--- src/router.jsx
+++ src/router.jsx
@@ -1,24 +1,44 @@
 import { createBrowserRouter, redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import MainLayout from "./pages/MainLayout";
+import Play from "./pages/Play";
+import LoginPage from "./pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     loader: () => {
-      return redirect('/home')
-    }
+      return redirect("/home");
+    },
   },
   {
-    path: '/',
-    element: <MainLayout/>,
+    path: "/",
+    element: <MainLayout />,
     children: [
       {
         path: "/home",
         element: <Home />,
       },
-    ]
-  }
-])
+      {
+        path: "/play",
+        element: <Play />,
+        loader: () => {
+          if (!localStorage.getItem("username")) {
+            return redirect("/login");
+          }
+        },
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+        loader: () => {
+          if (localStorage.getItem("username")) {
+            return redirect("/play");
+          }
+        },
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
