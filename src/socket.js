699b836ed4a33bda34972d6797ed52273b21ece6
yotamputra <yotamppy@gmail.com>
2024-12-05 10:18:15 +0700

    fix: change pointing domain to https

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+// const socket = io("http://localhost:3000", {
+//   autoConnect: false,
+// });
+
+const socket = io("https://yotamputra.site/", {
   autoConnect: false,
 });
 
